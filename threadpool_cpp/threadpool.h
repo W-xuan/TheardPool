764c7dde2d219224ba861a009f1497c3c6431bcf
魏煊 <49360064+W-xuan@users.noreply.github.com>
2022-09-07 07:15:28 +0800

    update

--- threadpool_cpp/threadpool.h
+++ threadpool_cpp/threadpool.h
@@ -4,41 +4,7 @@
 #include <iostream>
 #include <queue>
 #include <thread>
-using callback = void(*)(void*);
-struct Task
-{
-    Task()
-    {
-        function = nullptr;
-        arg = nullptr;
-    }
-    Task(callback f, void* arg)
-    {
-        function = f;
-        this->arg = arg;
-  
-    }
-    callback function; 
-    void* arg;
-};
-
-class TaskQueue
-{
-public:
-    TaskQueue();
-    ~TaskQueue();
-
-    void addTask(Task& task);
-    void addTask(callback func, void* arg);
-
-    Task takeTask();
-    inline int taskNumber()
-    {
-        return m_queue.size();
-    }
-private:
-    pthread_mutex_t m_mutex;
-    std::queue<Task> m_queue;
-};
+#include <cstring>
+#include <unistd.h>
 
 #endif
